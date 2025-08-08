8cf45b4ff8e3382b4291052c0e1cb4f2bfbdae91
denukavihisara10-jpg <denukavihisara10@gmail.com>
2025-08-08 18:39:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "ENTER YOUR SESSION ID":process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined? "N70G1DYC#L6_Ovczo-J45YJynJbNWvEzBD_B7b87BZg3pUe8VxHU":process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "mongodb://mongo:TPfocqWbSJrVJWbzhyXyYfMcsxszqiaQ@turntable.proxy.rlwy.net:59855",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
